b0bb1770f017de5e7ee3da1f136a54df627ee108
Flamerider347 <flamerider995@gmail.com>
2025-02-10 21:35:34 +1300

    Movement update 10/02
    Added flipping mechanic
    
    Added WASD controls
    
    Added basic tilemap with collisions
    
    Added Player

---
+++ year12minijam/scripts/character.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var flip = 1
+const gravity = 700
+const SPEED = 3
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	_movement(delta)
+
+
+func _movement(delta:float):
+	if Input.is_action_just_pressed("player_flip"):
+		flip *= -1
+	# Add the gravity.
+	if not is_on_floor() and flip == 1:
+		velocity.y += gravity * delta * flip
+	if not is_on_ceiling() and flip == -1:
+		velocity.y += gravity * delta * flip
+	
+	if Input.is_action_pressed("player_left"):
+		position.x -= SPEED
+	if Input.is_action_pressed("player_right"):
+		position.x += SPEED
+	# Handle jump.
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("player_jump") and is_on_ceiling():
+		velocity.y = JUMP_VELOCITY * flip
+	move_and_slide()
