65d736d7bf4f24d08c3131762e4a5a26a1004a46
Flamerider347 <flamerider995@gmail.com>
2025-02-10 21:54:48 +1300

    Movement update
    Fixed bugs where you could stick to ceiling
    
    Added flip cooldown
    
    Made screen size better
    
    Added some parkour
    
    Changed jumping mechanic so you can hold jump

--- year12minijam/scripts/character.gd
+++ year12minijam/scripts/character.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
+var canflip = true
 var flip = 1
 const gravity = 700
 const SPEED = 3
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +12,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _movement(delta:float):
-	if Input.is_action_just_pressed("player_flip"):
+	if Input.is_action_just_pressed("player_flip") and canflip:
+		canflip = false
+		$flip_timer.start()
 		flip *= -1
 	# Add the gravity.
 	if not is_on_floor() and flip == 1:
@@ -24,8 +27,12 @@ func _movement(delta:float):
 	if Input.is_action_pressed("player_right"):
 		position.x += SPEED
 	# Handle jump.
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+	if Input.is_action_pressed("player_jump") and is_on_floor() and flip == 1:
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("player_jump") and is_on_ceiling():
+	if Input.is_action_pressed("player_jump") and is_on_ceiling() and flip == -1:
 		velocity.y = JUMP_VELOCITY * flip
 	move_and_slide()
+
+
+func _on_flip_timer_timeout() -> void:
+	canflip = true
