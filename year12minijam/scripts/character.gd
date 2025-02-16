d16a1a58a9e368a9e18089c6b17fac1df8311ff5
22218-Burnside <22218@burnside.school.nz>
2025-02-16 16:46:03 +1300

    Added more Sound Effects
    Added sounds effects when walking and inverting gravity.
    Changed the SFX folder name to Sound

--- year12minijam/scripts/character.gd
+++ year12minijam/scripts/character.gd
@@ -6,6 +6,10 @@ const gravity = 700
 const SPEED = 6
 const JUMP_VELOCITY = -500.0
 
+#Sound effect variables
+@onready var gravity_inverted = $gravity_inverted
+@onready var walking_forest = $walking_forest
+
 
 func _physics_process(delta: float) -> void:
 	_movement(delta)
@@ -17,8 +21,10 @@ func _movement(delta:float):
 		$flip_timer.start()
 		if flip == 1:
 			$Sprite2D.flip_v = true
+			gravity_inverted.play()
 		else:
 			$Sprite2D.flip_v = false
+			gravity_inverted.play()
 		flip *= -1
 	# Add the gravity.
 	if not is_on_floor() and flip == 1:
@@ -28,8 +34,12 @@ func _movement(delta:float):
 	
 	if Input.is_action_pressed("player_left"):
 		position.x -= SPEED
+		walking_forest.play() 
+		# Right now, there is no way to switch the SFX between scenes yet.
+		# This means the forest_SFX will play throughout all scenes.
 	if Input.is_action_pressed("player_right"):
 		position.x += SPEED
+		walking_forest.play()
 	# Handle jump.
 	if Input.is_action_pressed("player_jump") and is_on_floor() and flip == 1:
 		velocity.y = JUMP_VELOCITY
