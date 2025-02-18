69d74449df2bf7594f13ea40127136b5ce162ea8
Flamerider347 <flamerider995@gmail.com>
2025-02-18 22:19:07 +1300

    Moving enemy late night update
    Added some character animations
    
    Added killable enemy
    
    Added some (2) code comments for enemy

--- year12minijam/scripts/character.gd
+++ year12minijam/scripts/character.gd
@@ -9,6 +9,7 @@ var can_sprint = true
 var running = false
 var release_sprint = true
 var jump_reset = true
+var squish_power = 1
 const JUMP_VELOCITY = -500.0
 
 
@@ -42,6 +43,7 @@ func _movement(delta:float):
 		release_sprint =true	
 		$sprint_timer.start(1)
 	if Input.is_action_just_pressed("player_flip") and canflip:
+		squish_power = 3
 		canflip = false
 		$flip_timer.start()
 		if flip == 1:
@@ -56,14 +58,19 @@ func _movement(delta:float):
 	if not is_on_ceiling() and flip == -1:
 		velocity.y += gravity * delta * flip
 	if Input.is_action_pressed("player_left"):
+		$AnimatedSprite2D.play("Run")
 		position.x -= SPEED
 		walking_forest.play()
 		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_pressed("player_right"):
+		$AnimatedSprite2D.play("Run")
 		position.x += SPEED
 		walking_forest.play() 
 		$AnimatedSprite2D.flip_h = false
-	# Handle jump.
+	if not Input.is_action_pressed("player_left") and not Input.is_action_pressed("player_right") and is_on_floor():
+		$AnimatedSprite2D.play("Idle")
+	if is_on_floor() or is_on_ceiling() and squish_power == 3:
+		squish_power = 1
 	if Input.is_action_pressed("player_jump") and is_on_floor() and flip == 1:
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_pressed("player_jump") and is_on_ceiling() and flip == -1:
@@ -72,10 +79,8 @@ func _movement(delta:float):
 		velocity.x = 0
 	move_and_slide()
 
-
 func _on_flip_timer_timeout() -> void:
 	canflip = true
-
 
 func _on_sprint_timer_timeout() -> void:
 	can_sprint = true
--- year12minijam/scripts/enemy.gd
+++ year12minijam/scripts/enemy.gd
@@ -1,12 +1,28 @@
 extends Node2D
 
 var speed = 0.001
-
+var damage = 0
+var health = 3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	#If health is below 1 enemy is removed
+	if health < 1:
+		queue_free()
 	$Path2D/PathFollow2D.progress_ratio += speed
+
+
+func _on_squish_hitbox_area_entered(area: Area2D) -> void:
+	#Checks if it's player, takes squish power and applies damage
+	if area.name == "player_squish":
+		damage = get_parent().get_node("forest/character").squish_power
+		health -= damage
+		damage = 0
+
+
+func _on_kill_hitbox_area_entered(area: Area2D) -> void:
+	pass
