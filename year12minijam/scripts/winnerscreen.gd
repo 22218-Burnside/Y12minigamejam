30e812658eb6f521ab414d6b8f3bb98fbdeaf8ae
Ben <22218@burnside.school.nz>
2025-02-20 17:50:42 +1300

    Added a background to winner screen and a quit option

--- year12minijam/scripts/winnerscreen.gd
+++ year12minijam/scripts/winnerscreen.gd
@@ -5,3 +5,7 @@ extends Control
 #when the button is pressed the screen is changed to the menu
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Menu.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
