6c28afae1fed08e7ae88acafdd8c031f4f006407
Flamerider347 <flamerider995@gmail.com>
2025-02-25 14:40:27 +1300

    Menu BG colour change

--- year12minijam/scripts/menu.gd
+++ year12minijam/scripts/menu.gd
@@ -1,6 +1,17 @@
 extends Control
+var noon_transparency = 1 
+var transparency_flip = 1
 
-#when the button is pressed the screen is changed to the starter level
+func _process(_delta: float) -> void:
+	$noon.modulate.a = noon_transparency * 0.01
+	if noon_transparency <= 80 and transparency_flip == 1:
+		noon_transparency += 0.1
+	if noon_transparency > 80 and transparency_flip == 1:
+		transparency_flip = -1
+	if noon_transparency >= 2 and transparency_flip == -1:
+		noon_transparency -= 0.1
+	if noon_transparency < 2 and transparency_flip == -1:
+		transparency_flip = 1
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/cave.tscn")
 
