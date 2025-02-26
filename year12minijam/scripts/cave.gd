154b9ac469ceddf07dc5c683e3983d60598c83a9
Flamerider347 <flamerider995@gmail.com>
2025-02-27 08:48:50 +1300

    Save

--- year12minijam/scripts/cave.gd
+++ year12minijam/scripts/cave.gd
@@ -12,12 +12,6 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/forest.tscn")
-
-
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "character":
 		get_tree().change_scene_to_file("res://scenes/forest.tscn")
