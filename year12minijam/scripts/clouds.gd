a4d3a795294bb62e2c48dbd82bd362b9fdc36d0b
Flamerider347 <flamerider995@gmail.com>
2025-02-28 09:47:29 +1300

    Save

--- year12minijam/scripts/clouds.gd
+++ year12minijam/scripts/clouds.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _ready() -> void:
-	$character/Camera2D.limit_top = 0
-	$character/Camera2D.limit_bottom = 1080
+	$character/Camera2D.limit_top = -30
+	$character/Camera2D.limit_bottom = 1100
 	# This gives the animation to the clouds behind the player
 	$parallax_back/layer1/layer1.play("default")
 	$parallax_back/layer2/layer2.play("default")
@@ -12,4 +12,5 @@ func _ready() -> void:
 	#This gives the animation to the clouds in front of the player
 	$parallax_front/layer4/layer4.play("default")
 	$parallax_front/layer6/layer6.play("default")
+	
 	
