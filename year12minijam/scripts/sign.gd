f9b1714e836dc7fe9ce0b92a113a79e64181c491
Flamerider347 <flamerider995@gmail.com>
2025-03-02 16:32:18 +1300

    Signs & Skulls 2: Electric Boogaloo
    - Added enlargeable signs
    - Added Skull signs
    - Added 2 signs with text
    - Changed Font to Pixel font

--- year12minijam/scripts/sign.gd
+++ year12minijam/scripts/sign.gd
@@ -1,6 +1,7 @@
 extends Area2D
-var can_read
-var reading = true
+var can_read = false
+var reading = false
+var characters_visible = 0
 signal player_reading
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,21 +9,27 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	$Sprite2D2/Label.visible_characters = round(characters_visible)
 	if reading:
+		characters_visible += 0.2
 		$Sprite2D2.show()
 	else:
 		$Sprite2D2.hide()
+		characters_visible = 0
+	if reading and Input.is_action_pressed("cancel_read"):
+		$Sprite2D2.hide()
+		reading = false
 	if Input.is_action_just_pressed("player_interact") and can_read:
 		reading = true
 		player_reading.emit()
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Character":
+	if body.name == "character":
 		can_read = true
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Character":
+	if body.name == "character":
 		can_read = false
